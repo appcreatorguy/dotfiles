a6de995db7093e43d14b3419faeaa5c40b3440a5
Manas Mengle <manmenonsense@gmail.com>
2022-05-31 19:33:43 +0100

    new colors

---
+++ .xmonad/lib/Colors/Latte.hs
@@ -0,0 +1,44 @@
+module Colors.Latte where
+
+import XMonad
+
+colorScheme = "latte"
+
+colorBack = "#e6e9ef"
+
+colorFore = "#4c4f69"
+
+color01 = "#eff1f5"
+
+color02 = "#d20f39"
+
+color03 = "#40a02b"
+
+color04 = "#df8e1d"
+
+color05 = "#1e66f5"
+
+color06 = "#ea76cb"
+
+color07 = "#209fb5"
+
+color08 = "#6c6f85"
+
+color09 = "#dce0e8"
+
+color10 = "#fe640b"
+
+color11 = "#7c7f93"
+
+color12 = "#8c8fa1"
+
+color13 = "#9ca0b0"
+
+color14 = "#8839ef"
+
+color15 = "#acb0be"
+
+color16 = "#5c5f77"
+
+colorTrayer :: String
+colorTrayer = "--tint 0xe6e9ef"
---
+++ .xmonad/lib/Colors/Mocha.hs
@@ -0,0 +1,44 @@
+module Colors.Mocha where
+
+import XMonad
+
+colorScheme = "mocha"
+
+colorBack = "#1e1e2e"
+
+colorFore = "#cdd6f4"
+
+color01 = "#1e1e2e"
+
+color02 = "#f38ba8"
+
+color03 = "#a6e3a1"
+
+color04 = "#f9e2af"
+
+color05 = "#89b4fa"
+
+color06 = "#f5c2e7"
+
+color07 = "#74c7ec"
+
+color08 = "#a6adc8"
+
+color09 = "#11111b"
+
+color10 = "#fab387"
+
+color11 = "#9399b2"
+
+color12 = "#7f849c"
+
+color13 = "#6c7086"
+
+color14 = "#cba6f7"
+
+color15 = "#585b70"
+
+color16 = "#bac2de"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x1e1e2e"
