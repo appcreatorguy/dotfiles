42a9f5869b602e7c11c5c694663c0ce9cd00ea9d
Manas Mengle <manmenonsense@gmail.com>
2022-08-08 16:35:06 +0100

    nvim: make guifont smaller

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -2,4 +2,4 @@ require "custom.autocmds"
 
 vim.cmd "silent! command! EnableShade lua require('shade').toggle()"
 vim.cmd "silent! command! TSEnable rainbow"
-set guifont=SauceCodePro\ Nerd\ Font:h12
+set guifont=SauceCodePro\ Nerd\ Font:h11
