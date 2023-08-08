116b69836f3f42e5a88465d8e279ba066703772f
Manas Mengle <manmenonsense@gmail.com>
2023-08-08 15:57:37 +0100

    Polybar: new layout

--- .config/polybar/bsp-layout-change.sh
+++ .config/polybar/bsp-layout-change.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$1" = "next" ]]; then
-  /usr/local/bin/bsp-layout next --layouts tiled,tall,monocle,grid,wide
+	/usr/local/bin/bsp-layout next --layouts tiled,tall,monocle,grid,wide,rwide,even
 elif [[ "$1" = "previous" ]]; then
-  /usr/local/bin/bsp-layout previous --layouts tiled,tall,monocle,grid,wide
+	/usr/local/bin/bsp-layout previous --layouts tiled,tall,monocle,grid,wide,rwide,even
 fi
